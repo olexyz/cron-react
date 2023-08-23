23e42f38abafec0402871f82ea03e41636fd0fb6
Olesya Danilova <olesiakk@gmail.com>
2023-08-23 16:03:48 +0300

    tabs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Main from "./components/Main";
 import {useState} from "react";
+import CronTabs from "./components/Tabs";
 
 function App() {
-  const [value, setValue] = useState('* * * * * * *');
+    const [value, setValue] = useState('* * * * * * *');
+    const createCronString = (newValue) =>{
+        console.log(newValue)
+        setValue(newValue);
+    }
   return (
     <div className="App">
       <Main value={value}/>
+      <CronTabs change={createCronString} />
     </div>
   );
 }
